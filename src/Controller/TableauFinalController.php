e41056f659cbb47a065f00df2d1326513aa20afe
adeloffre <antoine.deloffre2018@ampus-eni.fr>
2019-12-13 22:13:38 +0100

    Remplissage des deux tableaux

--- src/Controller/TableauFinalController.php
+++ src/Controller/TableauFinalController.php
@@ -2,31 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\JoueurRepository;
 use App\Repository\TournoiRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TableauFinalController extends AbstractController
 {
+    private $principale = array();
+    private $consolante = array();
     /**
      * @Route("/tableauFinal/{id}", name="tableauFinal")
      */
-    public function generationTableaux($id, TournoiRepository $tournoiRepository)
+    public function generationTableaux($id, TournoiRepository $tournoiRepository, JoueurRepository $joueurRepository)
     {
+        $this->remplirTableau($id, $tournoiRepository, $joueurRepository);
+
+        return $this->render('tableau_final\tableau_final.html.twig', [
+            'principale' => $this->principale,
+            'consolante' => $this->consolante,
+        ]);
+    }
+
+    public function remplirTableau($id, TournoiRepository $tournoiRepository, JoueurRepository $joueurRepository){
         $tournoi = $tournoiRepository->find($id);
-        $joueurs = $tournoi->getJoueurs();
-        $principale = array();
-        $consolante = array();
+        $joueurs = $joueurRepository->findByTournoi($tournoi);
+
         // On a uniquement ceux qui sont cochés. il faut donc récupérer ces joueurs et
         // ajouter le reste à l'autre tableau.
         foreach ($_POST as $key => $value){
-            var_dump($key.$value);
+            $joueur = $joueurRepository->find($key);
+            array_push($this->principale, $joueur);
+            unset($joueurs[array_search($joueur,$joueurs)]);
         }
-
-        return $this->render('tableau_final\tableau_final.html.twig', [
-            'principale' => $principale,
-            'consolante' => $consolante,
-        ]);
+        $this->consolante = array_values($joueurs);
     }
 
 
@@ -35,7 +44,7 @@ class TableauFinalController extends AbstractController
         // On est généreux, on imagine qu'un jour il y aura 128 personnes dans le tableau final
         $puiss2 = [1, 2, 4, 8, 16, 32, 64, 128];
 
-        // On parcourt les puissance de 2
+        // On parcourt les puissances de 2
         for ($i = 0; $i < sizeof($puiss2); $i++) {
             // Si par chance on tombe sur un nombre de joueur qui vaut une puissance de 2
             // on retourne cette puissance
